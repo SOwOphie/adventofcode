ee86f4c851e520efb7184753addd9444fa9c595f
Sophie Hirn <sophie.hirn@wyvernscale.com>
2021-12-02 19:46:58 +0100

    lua version now 100% safer

--- 2-1.lua
+++ 2-1.lua
@@ -3,12 +3,14 @@
 local x = 0
 local d = 0
 
-function forward(n) x = x + n end
-function up(n) d = d - n end
-function down(n) d = d + n end
+local env = {
+	forward = function(n) x = x + n end,
+	up = function(n) d = d - n end,
+	down = function(n) d = d + n end,
+}
 
 for l in io.lines("2.txt") do
-	load(l:gsub(' ', '('):gsub('$', ')'))()
+	load(l:gsub(' ', '('):gsub('$', ')'), "2.txt", "t", env)()
 end
 
 print(x*d)
--- 2-2.lua
+++ 2-2.lua
@@ -4,12 +4,14 @@ local a = 0
 local x = 0
 local d = 0
 
-function forward(n) x = x + n; d = d + a*n end
-function up(n) a = a - n end
-function down(n) a = a + n end
+local env = {
+	forward = function(n) x = x + n; d = d + a*n end,
+	up = function(n) a = a - n end,
+	down = function(n) a = a + n end,
+}
 
 for l in io.lines("2.txt") do
-	load(l:gsub(' ', '('):gsub('$', ')'))()
+	load(l:gsub(' ', '('):gsub('$', ')'), "2.txt", "t", env)()
 end
 
 print(x*d)
